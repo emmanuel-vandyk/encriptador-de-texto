281e5baab7d7a04ea70b71c08c1221a170127f05
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2023-02-04 22:58:23 -0300

    "Agregar funcionalidad en el archivo main.js"

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* Funcion encriptadora */
+function encriptarTexto() {
+    var x = '';
+
+    switch(x) {
+        case 'a':
+            console.log('enter');
+        case 'e':
+            console.log('ai');
+        case 'i':
+            console.log('imes');
+        case 'o':
+            console.log('ober');
+        case 'u':
+            console.log('ufat');
+        break;
+    }
+}
