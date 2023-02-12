957b1a12b58792ed42f704b79c60dd439e5494a7
Emmanuel-VanDick9 <emmaaavd@gmail.com>
2023-02-12 05:31:15 -0300

    Javascript terminado

--- js/main.js
+++ js/main.js
@@ -1,18 +1,74 @@
-/* Funcion encriptadora */
-function encriptarTexto() {
-    var x = '';
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const copia = document.querySelector(".btn-copiar");
 
-    switch(x) {
-        case 'a':
-            console.log('enter');
-        case 'e':
-            console.log('ai');
-        case 'i':
-            console.log('imes');
-        case 'o':
-            console.log('ober');
-        case 'u':
-            console.log('ufat');
-        break;
+copia.style.display = "none";
+
+/* Función para validar el texto */
+function validarTexto() {
+    let textoEscrito = document.querySelector(".text-area").value;
+    let validador = textoEscrito.match(/^[a-z]*$/);
+
+    if (validador || validador === 0) {
+        alert("Solo se permiten letras minúsculas y sin acentos");
+        location.reload();
+        return true;
     }
+}
+
+/* Función encriptadora del boton */
+
+function btnEncriptar() {
+    if (!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        mensaje.style.backgroundImage = "none";
+        textArea.value = "";
+        copia.style.display = "block";
+    }
+}
+
+/* Función para encriptar */
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+/* Función del botón desencriptadora */
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+}
+
+/* Función para desencriptar */
+
+function desencriptar(cadenaDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    cadenaDesencriptada = cadenaDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(cadenaDesencriptada.includes(matrizCodigo[i][1])) {
+            cadenaDesencriptada = cadenaDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return cadenaDesencriptada;
+}
+
+/* Función para copiar el texto encriptado */
+
+function copiar() {
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value);
+    mensaje.value = "";
+    alert("Texto copiado al portapapeles");
 }
